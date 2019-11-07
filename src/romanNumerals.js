66a39b4adf0a34dc787839ad927597698ba8244c
RANGANATHAN Yuvaraj <yuvaraj.ranganathan@bnpparibasfortis.com>
2019-11-07 16:42:52 +0100

    Rermove duplication fr0m 6 to 8  in the list

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,18 +1,21 @@
-class RomanNumerals{
-    convert(number){
+class RomanNumerals {
+    convert(number) {
         var romans = {
-            1 : "I",
-            4 : "IV",
-            5 : "V",
-            6 : "VI",
-            7 : "VII",
-            8 : "VIII",
-            9 : "IX"         
+            1: "I",
+            4: "IV",
+            5: "V",
+            9: "IX"
         };
-        if(number < 4){
+        if (romans[number]) {
+            return romans[number];
+        }
+
+        if (number < 4) {
             return romans[1].repeat(number);
         }
-        return romans[number];
+
+        return romans[5] + romans[1].repeat(number - 5);
+
     }
 }
 module.exports = RomanNumerals;
