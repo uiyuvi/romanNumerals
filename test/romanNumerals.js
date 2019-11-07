dc61693cb76d195fee94077c495e3b72ba7f5239
RANGANATHAN Yuvaraj <yuvaraj.ranganathan@bnpparibasfortis.com>
2019-11-07 15:57:41 +0100

    Refactor: Move convert logic to a function

--- test/romanNumerals.js
+++ test/romanNumerals.js
@@ -2,20 +2,19 @@ const assert = require("chai").assert
 const RomanNumerals = require('../src/romanNumerals');
 
 describe('roman numerals', function () {    
+    var romanNumerals = new RomanNumerals();
+    function convert(number) {
+        return romanNumerals.convert(number);
+    }
     it('should return I for 1', function () {
-        var romanNumerals = new RomanNumerals();
-        var convertedValue;
 
-        convertedValue = romanNumerals.convert(1);
-
-        assert.equal(convertedValue, 'I');
+        assert.equal(convert(1), 'I');
     });   
     it('should return II for 2', function () {
-        var romanNumerals = new RomanNumerals();
-        var convertedValue;
 
-        convertedValue = romanNumerals.convert(2);
-
-        assert.equal(convertedValue, 'II');
+        assert.equal(convert(2), 'II');
     });
+    
 });
+
+
