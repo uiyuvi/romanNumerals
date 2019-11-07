a90c0703bf4d062b2caad2f518f103f8d9374254
RANGANATHAN Yuvaraj <yuvaraj.ranganathan@bnpparibasfortis.com>
2019-11-07 16:32:53 +0100

    Should convert single digit Romans

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,14 +1,17 @@
 class RomanNumerals{
     convert(number){
-        var romans = {
+        var singleDigitRomans = {
             1 : "I",
             2 : "II",
             3 : "III",
             4 : "IV",
             5 : "V",
-            6 : "VI"
+            6 : "VI",
+            7 : "VII",
+            8 : "VIII",
+            9 : "IX"            
         };
-        return romans[number];
+        return singleDigitRomans[number];
     }
 }
 module.exports = RomanNumerals;
--- test/romanNumerals.js
+++ test/romanNumerals.js
@@ -1,28 +1,31 @@
 const assert = require("chai").assert
 const RomanNumerals = require('../src/romanNumerals');
 
-describe('roman numerals', function () {    
+describe('roman numerals', function () {
     var romanNumerals = new RomanNumerals();
     function convert(number) {
         return romanNumerals.convert(number);
     }
-    it('should return I for 1', function () {
-        assert.equal(convert(1), 'I');
-    });   
-    it('should return II for 2', function () {
-        assert.equal(convert(2), 'II');
-    });
-    it('should return III for 3', function () {
-        assert.equal(convert(3), 'III');
-    });
-    it('should return IV for 4', function () {
-        assert.equal(convert(4), 'IV');
-    });
-    it('should return V for 5', function () {
-        assert.equal(convert(5), 'V');
-    });
-    it('should return VI for 6', function () {
-        assert.equal(convert(6), 'VI');
+    describe('single digit romans', () => {
+        var testValues = [
+            { input: 1, expected: "I" },
+            { input: 2, expected: "II" },
+            { input: 3, expected: "III" },
+            { input: 4, expected: "IV" },
+            { input: 5, expected: "V" },
+            { input: 6, expected: "VI" },
+            { input: 7, expected: "VII" },
+            { input: 8, expected: "VIII" },
+            { input: 9, expected: "IX" }
+        ];
+
+        testValues.forEach(function (test) {
+            it(`gives ${test.expected} for ${test.input}`, function () {
+                assert.equal(test.expected, convert(`${test.input}`));
+            });
+        }
+        );
+
     });
 });
 
